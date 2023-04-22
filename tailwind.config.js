e9427de29a85fbdea58a8f9be4d2416c6f4f2204
Juane003 <j.emilio164@gmail.com>
2023-04-22 20:40:57 -0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        gradient: "gradient 10s ease infinite",
+      },
+      keyframes: {
+        gradient: {
+          "0%, 100%": {
+            "background-size": "400% 400%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
